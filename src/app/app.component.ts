ea1d9706ca372c74cf9449def3defe39a67f2ac9
Ruma303 <matteoh8@hotmail.it>
2024-05-02 16:49:27 +0200

    reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, AboutComponent],
-  // templateUrl: './app.component.html',
-  template: `
-    <h1> Angular </h1>
-    <app-welcome />
-    <app-about />
-  `,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-start';
+  
 }
